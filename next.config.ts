bb14f890a097dd423f3137aa7c893abc7b753f4a
HuyMinhDev <nguyenminhhuy2410@gmail.com>
2026-08-29 15:45:58 +0700

    Fix build vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'res.cloudinary.com' }],
+  },
 };
 
 export default nextConfig;
